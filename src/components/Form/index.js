449f393e6b38b885e5ed6c00dc57d56ad3fdd317
Natalia-Timote <nataliamirian.nt@gmail.com>
2025-01-04 02:19:21 -0300

    criando botao e validando

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -15,14 +15,19 @@ const Form = () => {
         'Inovação e gestão'
     ]
 
+    const whenSaving = (event) => {
+        event.preventDefault();
+        console.log("Form foi submetido");
+    }
+
     return (
         <section className="form">
-            <form>
+            <form onSubmit={whenSaving}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <TextField label="Nome" placeholder="Digite seu nome" />
-                <TextField label="Cargo" placeholder="Digite seu cargo" />
+                <TextField required="true" label="Nome" placeholder="Digite seu nome" />
+                <TextField required="true" label="Cargo" placeholder="Digite seu cargo" />
                 <TextField label="Imagem" placeholder="Digite o endereço da imagem" />
-                <DropdownList label="Time" items={teams} />
+                <DropdownList required="true" label="Time" items={teams} />
                 <Button>Criar Card</Button>
             </form>
         </section>
